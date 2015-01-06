d24ec9cc99cd27ab1bb93759f152d7b5bb65037e
ilfroloff <ilfroloff@gmail.com>
2015-01-06 19:51:51 +0300

    - middleware: remade rest middleware

--- core/middleware/rest.js
+++ core/middleware/rest.js
@@ -1,104 +1,106 @@
-//const STATUS = {
-//    ok: 0,
-//    fail: 1,
-//    error: 2
-//};
-//
-//var requestPopulate = function(request) {
-//    if(request.method === 'GET') {
-//        request.data = request.query;
-//    } else {
-//        request.data = request.body;
-//    }
-//};
-//var responsePopulate = function(response) {
-//    response.ok = function(data) {
-//        var responseData = {status: STATUS.ok, version: config.VERSION};
-//        if(data) {
-//            responseData.data = data;
-//        }
-//        response.json(data);
-//        //response.json(responseData);
-//    };
-//    response.fail = function(key) {
-//        response.json({
-//            status: STATUS.fail,
-//            data: key
-//        });
-//    };
-//    response.err = response.error = function(err) {
-//        console.log(err);
-//        throw err;
-//        response.json({
-//            status: STATUS.error,
-//            version: config.VERSION,
-//            data: err
-//        });
-//    };
-//};
-//var REST = function(request, response, next) {
-//    if(
-//        typeof request.data === 'undefined' &&
-//        typeof response.ok === 'undefined' &&
-//        typeof response.fail === 'undefined' &&
-//        typeof response.err === 'undefined' &&
-//        typeof response.error === 'undefined'
-//    ) {
-////    console.log('json', response.json);
-//        requestPopulate(request);
-//        responsePopulate(response);
-//    } else {
-//        throw 'Some module rewrite response';
-//    }
-//    next();
-//};
-//module.exports = function() {
-//    return REST;
-//};
+var _ = require('lodash'),
 
-var request_populate = function(request) {
-    if(request.method === 'GET') {
-        request.data = request.query;
-    } else {
-        request.data = request.body;
-    }
+    request_populate = function(request) {
+        if(request.method === 'GET') {
+            request.data = request.query;
+        } else {
+            request.data = request.body;
+        }
 
-    if(
-        Object.prototype.toString.call(request.data) === '[object Object]' &&
-        !Object.keys(request.data).length
-    ) {
-        request.data = null;
-    }
-};
-var response_populate = function(response) {
-    response.ok = function(data) {
-        response.json(data);
-    };
-    response.fail = function(key) {
-        response.json({
-            status: 'fail',
-            data: key
-        });
-    };
-    response.err = response.error = function(err) {
-        throw err;
-    };
-};
-var REST = function(request, response, next) {
-    if(
-        typeof request.data === 'undefined' &&
-        typeof response.ok === 'undefined' &&
-        typeof response.fail === 'undefined' &&
-        typeof response.err === 'undefined' &&
-        typeof response.error === 'undefined'
-    ) {
+        if(
+            Object.prototype.toString.call(request.data) === '[object Object]' &&
+            !Object.keys(request.data).length
+        ) {
+            request.data = null;
+        }
+    },
+    response_populate = function(response) {
+        var send = function(options) {
+            if(options.code) {
+                response.status(options.code);
+            }
+
+            response.send(options.resp);
+        };
+
+        response.ok = function(data) {
+            send({
+                resp: data
+            });
+        };
+        response.fail = function(key) {
+            send({ resp: {
+                status: 'fail',
+                data: key
+            } });
+        };
+        response.err = response.error = function(err) {
+            console.log(err);
+
+            send({
+                code: 500,
+                resp: 'Server Internal Error'
+            });
+        };
+
+        response.forbidden = function(data) {
+            send({
+                code: 403,
+                resp: data
+            });
+        };
+        response.not_found = response.notFound = function(data) {
+            send({
+                code: 404,
+                resp: data
+            });
+        };
+    },
+
+    REST;
+
+REST = function(request, response, next) {
+    var intersection = function() {
+            var args = [].slice.call(arguments),
+                intersected = [];
+
+            args[0].forEach(function(name) {
+                if(name in args[1]) {
+                    intersected.push(name);
+                }
+            });
+
+            return intersected;
+        },
+
+        request_populate_list = [
+            'data'
+        ],
+        rewrited_request_keys = intersection(request_populate_list, request),
+
+        response_populate_list = [
+            'ok',
+            'fail',
+            'err', 'error',
+
+            'forbidden',
+            'not_found', 'notFound'
+        ],
+        rewrited_response_keys = intersection(response_populate_list, response);
+
+    if(!rewrited_request_keys.length && !rewrited_response_keys.length) {
         request_populate(request);
         response_populate(response);
     } else {
-        throw 'Some module rewrite response';
+        throw _.template('Some module rewrite response. Request: <%= req_keys %>. Response: <%= res_keys %>')({
+            req_keys: rewrited_request_keys,
+            res_keys: rewrited_response_keys
+        });
     }
 
     next();
 };
 
-module.exports = function() { return REST };
+module.exports = function() {
+    return REST
+};
