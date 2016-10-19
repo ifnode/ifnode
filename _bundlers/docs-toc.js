dc8ef0e00454cc575c9b3c4df7a386da2f24b8df
Ilya Frolov <ilfroloff@gmail.com>
2016-10-19 12:14:06 +0300

    docs: adapted site to mobile devices (added responsive possibility)

---
+++ _bundlers/docs-toc.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Path = require('path');
+const convertMDtoHTML = require('./helpers/convertMDtoHTML');
+const read = require('./helpers/readUTF8File');
+const write = require('./helpers/writeUTF8File');
+
+Promise
+    .all([
+        read(Path.resolve(__dirname, './../_layouts/default.html')),
+        convertMDtoHTML(Path.resolve(__dirname, './../_pages/docs/toc.md'))
+    ])
+    .then(results => {
+        const layout = results[0];
+        const html = results[1];
+
+        return write(
+            Path.resolve(__dirname, './../docs/toc.html'),
+            layout
+                .replace('__MAIN_CONTENT_CLASS__', 'docs-toc')
+                .replace('__MAIN_CONTENT__', html)
+        );
+    })
+    .catch(err => console.error(err));
--- _bundlers/docs.js
+++ _bundlers/docs.js
@@ -16,32 +16,45 @@ Promise
         read(Path.resolve(__dirname, './../_layouts/default.html')),
         read(Path.resolve(__dirname, './../_layouts/docs.html')),
 
-        read(Path.resolve(docs_folder, './toc.md')).then(content => Marked(content)),
-        Promise.all(Diread({src: docs_folder}).map(filepath => read(filepath).then(content => ({
-            filepath,
-            content: Marked(content)
-        }))))
+        read(Path.resolve(docs_folder, './toc.md')),
+        Promise.all(
+            Diread({
+                src: docs_folder,
+                mask: filepath => filepath.search('toc.md') !== -1
+            }).map(filepath => read(filepath).then(content => ({
+                filepath,
+                content: Marked(content)
+            })))
+        )
     ])
     .then(results => {
         const default_layout = results[0];
         const doc_layout = results[1];
-        const toc = results[2];
+        const markdown_toc = results[2];
         const docs = results[3];
 
         return Promise.all(docs.map(doc => {
+            const relive_doc_path = doc.filepath.replace(docs_folder, '');
+            const resolve_current_page_from_toc_regexp_string = `\\[(.+?)\\]\\(\\/docs${relive_doc_path
+                .replace('.md', '')
+                .replace('/index', '')
+                .replace(/\//g, '\\/')
+                }\\)`;
+
             return write(
                     Path.resolve(
                         docs_output,
-                        doc.filepath
-                            .replace(docs_folder, '.')
-                            .replace('.md', '.html')
+                        `.${relive_doc_path.replace('.md', '.html')}`
                     ),
                     default_layout
                         .replace('__MAIN_CONTENT_CLASS__', 'docs')
                         .replace(
                         '__MAIN_CONTENT__',
                         doc_layout
-                            .replace('__TOC__', toc)
+                            .replace('__TOC__', Marked(markdown_toc.replace(
+                                new RegExp(resolve_current_page_from_toc_regexp_string),
+                                '**$1**'
+                            )))
                             .replace('__DOC_PAGE__', doc.content)
                     ))
             }
