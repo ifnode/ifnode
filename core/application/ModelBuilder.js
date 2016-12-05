f2db8407dfe053979da146d9fa1b19e3f1742c99
Ilya Frolov <ilfroloff@gmail.com>
2016-12-06 00:41:01 +0300

    general: added injection of application to ModelBuilder
    Moved application instance filling by models to model builder class

--- core/application/ModelBuilder.js
+++ core/application/ModelBuilder.js
@@ -19,7 +19,7 @@ function ModelBuilder(dao_list) {
 /**
  *
  * @param   {Object}    model_config
- * @param   {Object}    options
+ * @param   {Object}    [options]
  * @returns {Function}
  */
 ModelBuilder.prototype.make = function make(model_config, options) {
@@ -49,9 +49,10 @@ ModelBuilder.prototype.make = function make(model_config, options) {
 
 /**
  *
+ * @param   {Application}   app
  * @returns {Object.<string, Function>}
  */
-ModelBuilder.prototype.compile_models = function compile_models() {
+ModelBuilder.prototype.compile_models = function compile_models(app) {
     var model_prototypes = this._model_prototypes;
 
     return Object.keys(model_prototypes).reduce(function(models, model_unique_name) {
@@ -60,6 +61,7 @@ ModelBuilder.prototype.compile_models = function compile_models() {
         var options = model_prototype.options;
 
         models[model_unique_name] = compiled_model;
+        app.models[model_unique_name] = compiled_model;
 
         if(options.alias) {
             toArray(options.alias).forEach(function(alias) {
@@ -68,6 +70,7 @@ ModelBuilder.prototype.compile_models = function compile_models() {
                 }
 
                 models[alias] = compiled_model;
+                app.models[alias] = compiled_model;
             });
         }
 
