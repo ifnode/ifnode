c911965f3b5db486761ed28a12b59aef2374b629
Ilya Frolov <ilfroloff@gmail.com>
2016-12-21 00:38:06 +0300

    general: added default options from IFNode to Application

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ var _default_app_key = null;
 /**
  * Creates or/and return application instance
  *
- * @param   {string|ApplicationOptions} options
+ * @param   {string|ApplicationOptions} [options]
  * @returns {Application}
  */
-var IFNode = function(options) {
+function IFNode(options) {
     if(_default_app_key && !options) {
         return _applications_cache[_default_app_key];
     }
@@ -21,7 +21,7 @@ var IFNode = function(options) {
         return _applications_cache[options];
     }
 
-    var app = new Application(options);
+    var app = new Application(options || {});
     var key = app.alias || app.id;
 
     if(!_default_app_key) {
@@ -31,7 +31,7 @@ var IFNode = function(options) {
     _applications_cache[key] = app;
 
     return app;
-};
+}
 
 Object.defineProperty(IFNode, 'VERSION', {
     value: package_json.version
