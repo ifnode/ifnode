e7cf0cf8fd581c954e81d006bdcb75fb17ee7db1
Ilya Frolov <ilfroloff@gmail.com>
2017-02-06 13:55:15 +0300

    models: assign different _driver property to each DAO
    Incorrect defining of Schema driver (for each DAO must be unique driver connection)

---
+++ examples/models/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var IFNode = require('../../');
+
+module.exports = IFNode({
+    alias: 'models',
+    project_folder: __dirname
+});
--- test/04. models.js
+++ test/04. models.js
@@ -1,7 +1,18 @@
 'use strict';
 
-var app = require('../examples/extensions/app'),
-    should = require('should');
+var Should = require('should');
+var app = require('../examples/models/app');
 
 describe('Models', function() {
+    describe('app.Model(model_config: Object, [options: Object])', function() {
+        it('should exists', function() {
+            app.Model.should.be.an.Function;
+        });
+
+        it('should creates models', function() {
+            app.load();
+            Should.equal(app.models.FirstModel.sameMethod(), 1);
+            Should.equal(app.models.SecondModel.sameMethod(), 2);
+        });
+    });
 });
