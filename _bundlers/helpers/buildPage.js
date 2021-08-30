edfd12e634fa1d64e24f9084394005ecced72493
Illia Fralou <ilfroloff@gmail.com>
2021-08-30 18:57:48 +0300

    general: added Google Analytics, removed "https://nodesecurity.io"

--- _bundlers/helpers/buildPage.js
+++ _bundlers/helpers/buildPage.js
@@ -19,6 +19,7 @@ function buildPage(main_content, main_content_class, output) {
     return read(Path.resolve(__dirname, './../../_layouts/default.html'))
         .then(layout => layout
             .replace(/__SITE_VERSION__/g, package_json.version)
+            .replace(/__GA_TRACKING_ID__/g, package_json.settings.GA_TRACKING_ID)
             .replace('__MAIN_CONTENT_CLASS__', main_content_class)
             .replace('__MAIN_CONTENT__', main_content)
         )
